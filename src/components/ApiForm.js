6062d816833fed4386c58f4bf5df11f1cfbe8d23
edward <edward_gio@hotmail.com>
2023-04-20 11:44:27 -0500

    core structure ApiForm get method added to up2tom url

--- src/components/ApiForm.js
+++ src/components/ApiForm.js
@@ -1,9 +1,51 @@
-const ApiForm = () =>{
-    return(
-        <>
-        <p>Api content</p>
-        </>
-    );
-}
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const ApiForm = () => {
+  //   const [quote, setQuote] = useState("");
+  //   const getQuote = () => {
+  //     axios
+  //       .get("https://api.quotable.io/random")
+  //       .then((response) => {
+  //         console.log(response.data.content);
+  //         setQuote(response.data.content);
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //       });
+  //   };
+  //   return (
+  //     <>
+  //       <button onClick={getQuote}>Get data</button>
+  //       {quote}
+  //     </>
+  //   );
+  //
+
+  const [inputVariables, setInputVariables] = useState([]);
+
+  //   const url = "https://api.up2tom.com/models/58d3bcf97c6b1644db73ad12"; // General data
+  const url = "https://docs.up2tom.com/v3/models/58d3bcf97c6b1644db73ad12"; // CORS error
+
+  const getModel = () => {
+    axios
+      //   .get("https://api.up2tom.com/models/58d3bcf97c6b1644db73ad12", {
+      .get(url, {
+        headers: { Authorization: "Bearer 9307bfd5fa011428ff198bb37547f979" },
+      })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <>
+      <button onClick={getModel}>Get model</button>
+    </>
+  );
+};
 
 export default ApiForm;
